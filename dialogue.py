9dcf1c02a6b3d9fddd991e4d9f5e4ad42f789ec3
hsu-patty <pattyhsu615@gmail.com>
2022-11-30 19:34:26 -0800

    Add game dialogue to dialogue.py

--- dialogue.py
+++ dialogue.py
@@ -1,8 +1,30 @@
-intro = ""
 
-level_2_up = "explain headlights"
+intro_1 = "To find the treasure, you have to find to octopus hiding in the depths of the water. But in order to get\n"\
+          "strong enough to fight him, you'd need to level up your sub! To level your sub, you have to go to [!]\n" \
+          "symbols which indicate events. There are 3 types of events: battle, riddles, and choices. For battles,\n" \
+          "if you defeat the monster you gain exp and morale. But if you are defeated by the monster, game over.\n" \
+          "For riddle events, if you get the riddle right, crew morale and exp increases. If you get it wrong, \n" \
+          "your crew loses faith in you and morale decreases. In the cases of choice events, if you make the \n" \
+          "right choice, morale and exp will increase. If you make the wrong choice, morale will decrease.\n"
 
-level_3_up = "explain sonar"
+intro_2 = "There are 2 main ways to lose this game: losing all your crew morale or losing monster battles. \n" \
+          "Crew morale increases when you do well at events, and decreases if you run away or lose at events. \n" \
+          "So you definitely want to make sure you do well at events or your crew loses faith in you.\n" \
+          "We've talked about battle events, but again if you lose all your health in battle it’s game over. \n"\
+          "Don't worry though, everytime you finish a battle your hp comes back for the next battle \n" \
+
+intro_3 = "If you look at your submarine map right now, you can only see shallow waters. That’s because we’ve \n"\
+          "lost our submarine headlights somewhere in these waters. Gain the headlights by leveling up to level 2! \n" \
+
+level_2_up = "Congratulations on reaching level 2! You’ve successfully gotten your subs headlights back so you’re \n" \
+             "able to see slightly deeper into the ocean now. Your stats have leveled up and you’ll be able to \n" \
+             "fight tougher monsters. Be aware, these monsters deeper down are going to be tougher than before \n. " \
+             "In order to get to the octopus, you need to get to level 3 to get the sonar to find him"
+
+level_3_up = "You’ve reached level 3! Unfortunately, no matter your headlight strength, you won’t be able to see \n" \
+             "to the bottom. No worries though, with this sonar, you’ll be able to locate where the octopus is \n" \
+             "relative to you. You now have an extra sonar option to choose when you move. Try it out next when \n" \
+             "you select your move!"
 
 help = ""
 
